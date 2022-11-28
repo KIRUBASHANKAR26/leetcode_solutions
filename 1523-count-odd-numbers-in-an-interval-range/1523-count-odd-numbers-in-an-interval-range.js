36b05ca37b8655c707ece17409454683bb5afac7
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2022-11-28 23:21:05 +0530

    Time: 113 ms (57.69%), Space: 42.8 MB (6.20%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -4,21 +4,21 @@
  * @return {number}
  */
 ////Approach 1
-var countOdds = function(low, high) {
-    let count = 0;
-    for(i=low;i<=high;i++){
-        if(i%2===1){
-            count += 1;
-        }
-    }
-    return count;
-};
-////Approach 2
 // var countOdds = function(low, high) {
-//     if(low%2==0)
-//         low++;
-//     if(high%2==0)
-//         high--;
-//     console.log(high,low)
-//     return (high-low)/2 +1;
+//     let count = 0;
+//     for(i=low;i<=high;i++){
+//         if(i%2===1){
+//             count += 1;
+//         }
+//     }
+//     return count;
 // };
+//Approach 2
+var countOdds = function(low, high) {
+    if(low%2==0)
+        low++;
+    if(high%2==0)
+        high--;
+    console.log(high,low)
+    return (high-low)/2 +1;
+};
