9a714d00fd20eb5ca7ef09abafa5ceba91e37e2d
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2022-11-30 23:19:32 +0530

    Time: 88 ms (61.92%), Space: 42.1 MB (30.27%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+const arrSum = (salary) => {
+   return salary.reduce((a,b) => a + b, 0)
+}
+var average = function(salary) {
+    const max = Math.max(...salary);
+    const min = Math.min(...salary);
+    const length = salary.length -2;
+    const excludedSalary = arrSum(salary) - (max + min)
+    
+    return excludedSalary/length;
+};
