ebfccb8dc586dda6525b404b805f46228ac34a16
Kirubashankar K <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2022-12-12 22:43:49 +0530

    Time: 149 ms (5.11%), Space: 44.1 MB (9.80%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    console.log(n.toString(2).replaceAll("0","").length)
+console.log(n.toString(2).replaceAll("0","").length)
    return n.toString(2).split("0").join("").length;
 };
