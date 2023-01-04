e8975c49f2461e739da268c01d92512e6e6fcb65
Kirubakk <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2023-01-04 23:17:41 +0530

    Time: 182 ms (17.22%), Space: 46.7 MB (61.13%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let moveZeroes = function(nums) {
+        for(let i= nums.length-1; i>=0; i--){
+            if(nums[i]===0){
+                nums.push(0) 
+                nums.splice(i,1)
+            }
+        }
+        return nums
+    };
