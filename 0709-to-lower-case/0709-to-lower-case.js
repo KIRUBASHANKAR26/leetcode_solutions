0a0cb8eb1c9e1c4a4d8bba53337e46d4be8c5b64
Kirubakk <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2023-01-11 23:25:56 +0530

    Time: 63 ms (82.95%), Space: 42.3 MB (29.87%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.js
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -3,10 +3,10 @@
  * @return {string}
  */
 var toLowerCase = function(s) {
-    for(let i=0;i<s.length;i++){
-        if(s[i].charCodeAt(s[i])<=90 && s[i].charCodeAt(s[i])>=65){
-            s = s.replace(s[i],String.fromCharCode(s[i].charCodeAt(s[i]) + 32))
-        }
-    }
-    return s
+    // for(let i=0;i<s.length;i++){
+    //     if(s[i].charCodeAt(s[i])<=90 && s[i].charCodeAt(s[i])>=65){
+    //         s = s.replace(s[i],String.fromCharCode(s[i].charCodeAt(s[i]) + 32))
+    //     }
+    // }
+    return s.toLowerCase()
 };
