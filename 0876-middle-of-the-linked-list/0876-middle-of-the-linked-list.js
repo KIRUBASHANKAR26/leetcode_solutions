2516c1625cd7a7ce9b178a77ce7e4015b86af570
Kirubakk <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2023-01-21 20:37:19 +0530

    Time: 114 ms (5.33%), Space: 42.1 MB (49.32%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -11,7 +11,6 @@
  */
 var middleNode = function(head) {
     let slow = head, fast = head
-     
     while(fast !== null && fast.next !== null){
         slow = slow.next
         fast = fast.next.next
