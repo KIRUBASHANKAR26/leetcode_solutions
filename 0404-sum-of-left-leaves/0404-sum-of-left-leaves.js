b99cdfbf98118974e5b3110e406785862b05ef01
Kirubakk <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2023-01-22 18:55:19 +0530

    Time: 81 ms (42.41%), Space: 44.2 MB (62.81%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root,isLeft) {
+    let result = 0
+    if(!root){
+        return 0
+    }
+
+    if(root.val && isLeft && !root.left && !root.right){
+             console.log(root.val)
+        result = root.val 
+    }
+    
+    return result + sumOfLeftLeaves(root.left,true) + sumOfLeftLeaves(root.right,false);
+    
+};
