38844613325783e318ea9983c82d66c309efb80b
Kirubakk <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2023-02-15 23:52:54 +0530

    Time: 64 ms (68.65%), Space: 42 MB (47.61%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -2,20 +2,28 @@
  * @param {number[]} digits
  * @return {number[]}
  */
+// var plusOne = function(digits) {
+//     let isPlus = true
+//     for(let i=digits.length - 1;i>=0;i--){
+//         if(isPlus){
+//            if(digits[i]===9){
+//             digits[i] = 0
+//             }else{
+//                 digits[i] = digits[i] + 1;
+//                 isPlus = false
+//             } 
+//         }
+//         if(isPlus && i === 0){
+//             digits.unshift(1)
+//         }
+//     }
+//     return digits
+// };
+
 var plusOne = function(digits) {
-    let isPlus = true
-    for(let i=digits.length - 1;i>=0;i--){
-        if(isPlus){
-           if(digits[i]===9){
-            digits[i] = 0
-            }else{
-                digits[i] = digits[i] + 1;
-                isPlus = false
-            } 
-        }
-        if(isPlus && i === 0){
-            digits.unshift(1)
-        }
-    }
-    return digits
+    const fromArray = digits.join(''); // get Digits out of Array
+    const toInt = BigInt(fromArray) ; //Dont use parseInt, use BigInt instead :) 
+
+    const addOne = toInt + 1n; // add one! 
+    return addOne.toString().split('')
 };
